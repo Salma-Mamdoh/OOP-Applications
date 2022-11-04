0164034165185c63a84d8d4d2dc0714895ab87d8
Salma. Mamdoh sabry <52897508+Salma-Mamdoh@users.noreply.github.com>
2022-11-04 18:47:41 +0200

    V2

--- BigReal.h
+++ BigReal.h
@@ -22,6 +22,7 @@ public:
     BigReal(BigReal&& other);
     //BigReal& operator= (BigReal&& other);
     BigReal& operator= (BigReal&& other);
+    string add_zeros_toFraction(string s1, string s2);
     static int  getmax(string str1, string str2,string str3 ,string str4);
     string addition(string str1, string str2, string str3, string str4);
     string substraction(string str1, string str2, string str3, string str4);
@@ -30,4 +31,3 @@ public:
 
 };
 #endif 
-
